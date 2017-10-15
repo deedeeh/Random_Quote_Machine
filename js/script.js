5b25236b5811d485b1cbd62385c6cd22e9437392
Dina Hafez <dinahafez.8@gmail.com>
2017-10-15 20:23:02 +0100

    Add random quote machine repository
    The HTML has a title, div for the quotes to live in and the two
    buttons; one to change the quote and the other for share it on twitter.
    Also I tried to work with HTML5 tags; header, section and footer. I
    have started to add very few styles mainly to the body, still not sure
    about the styling yet. Also I have added the script file which is not
    fully working yet, in console log the random quotes are working but
    still to link that to the DOM with the button.

---
+++ js/script.js
@@ -0,0 +1,49 @@
+
+var quotes = [
+  {
+    quote: "Whatever I know how to do, I've already done. Therefore I must always do what I do not know how to do.",
+    author: "Eduardo Chillida"
+  },
+  {
+    quote: "The least of things with a meaning is worth more in life than the greatest things without it.",
+    author: "Carl Jung"
+  },
+  {
+    quote: "The only thing worse than being talked about is not being talked about.",
+    author: "Oscar Wilde"
+  },
+  {
+    quote: "We can control our lives by controlling our perceptions.",
+    author: "Bruce Lipton"
+  },
+  {
+    quote: "It is always the simple that produces the marvelous.",
+    author: "Amelia Barr"
+  },
+  {
+    quote: "Doubt is not a pleasant condition, but certainty is absurd.",
+    author: "Voltaire"
+  },
+  {
+    quote: "Your time is limited; so don't waste it living someone else's life. Don't be trapped by dogma - which is living with the results of other poeple's thinking. Don't let the noise of others' opinions drown out your own inner voice.",
+    author: "Steve Jobs"
+  },
+  {
+    quote: "If you have knowledge, let others light their candles in it.",
+    author: "Margaret Fuller"
+  }
+];
+
+function getRandomQuote() {
+  var randomArr = Math.floor(Math.random() * quotes.length);
+  var randomQuotes = quotes[randomArr];
+  // var wrapper = "<div id='quotes-wrapper'></div>";
+  // var quotesSection = document.getElementsByClassName("random-quotes");
+  // var wrapperEl = wrapper.appendChild(randomQuotes);
+  // var wrapperPosition = quotesSection.appendChild(wrapper);
+  // console.log();
+}
+
+document.getElementById("quotesWrapper").addEventListener("click", getRandomQuote);
+
+getRandomQuote();
